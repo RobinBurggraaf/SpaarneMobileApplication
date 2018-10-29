76791633f99a252e6eb29a132e6f5041c4494d7f
Peter Marcelis <peter.marcelis@hva.nl>
2018-10-29 19:37:33 +0100

    examples, gitignore, textmesh
    - Added 2 examples of how to  flood your scene and how to use scriptable objects
    
    -Added textmeshpro
    
    - Changed gitignore (was empty somehow)

---
+++ Assets/Scenes/TestScenes_Peter/TestScripts/TutorialObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "new tutorialObject", menuName = "TutorialObjects")]
+public class TutorialObject : ScriptableObject {
+
+    public string title;
+    public string description;
+
+    public Sprite image;
+}
