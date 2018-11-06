cb0d0061a692d6bb44985f4bb8d5ec4188cba97a
Peter Marcelis <peter.marcelis@hva.nl>
2018-11-06 14:46:26 +0100

    Added first screen test scene

---
+++ Assets/GridLayoutResizer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridLayoutResizer : MonoBehaviour
+{
+
+    public GameObject gridContainer;
+
+	void Start ()
+	{
+	    Vector2 gridSize = gridContainer.GetComponent<RectTransform>().rect.size;
+        Debug.Log("gridsize: " + gridSize);
+	    float height = (gridSize.y / 4) - (gridSize.y * .05f);
+	    float width = gridSize.x - (gridSize.x * .1f);
+        Vector2 newSize = new Vector2(gridSize.x, height);
+	    gridContainer.GetComponent<GridLayoutGroup>().cellSize = newSize;
+	}
+}
