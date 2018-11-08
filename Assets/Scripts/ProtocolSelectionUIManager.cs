3d719b643ec8df3fab3a94f552fd08fc46754ce1
Robin Burggraaf <robinburggraaf92@hotmail.com>
2018-11-08 14:29:45 +0100

    Script protocol added

---
+++ Assets/Scripts/ProtocolSelectionUIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProtocolSelectionUIManager : MonoBehaviour {
+
+    public GameObject MenuCanvas;
+    public GameObject SelectedProtocolCanvas;
+    public Text title;
+    public Text goal;
+    public Text materials;
+    public GameObject buttonAR;
+    public GameObject buttonVideo;
+    public Image titleBar;
+
+    public void ProtocolSelected(TutorialObject t){
+
+        title.text = t.title;
+        goal.text = t.goal;
+        materials.text = t.materials;
+
+        buttonAR.GetComponent<Image>().color = t.color;
+        //insert AR shit here
+        buttonVideo.GetComponent<Image>().color = t.color;
+        //insert Video shit here
+        titleBar.color = t.color;
+        SelectedProtocolCanvas.SetActive(true);
+        MenuCanvas.SetActive(false);
+    }
+
+
+}
---
+++ Assets/Scripts/TutorialObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+[CreateAssetMenu(fileName = "new protocol", menuName = "ProtocolObjects")]
+public class TutorialObject : ScriptableObject
+{
+
+    public Color32 color;
+    public string title;
+    public string goal;
+    public string materials;
+
+    public VideoClip video;
+}
